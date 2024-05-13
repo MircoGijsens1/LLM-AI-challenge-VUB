e5328b878d2f845f04d419fb83f167293d705221
MircoGijsens1 <mirco.gijsens@gmail.com>
2024-05-14 01:14:08 +0200

    Add files via upload

---
+++ automl/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import InputWindow from "./components/InputWindow";
+
+function App() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <InputWindow />
+    </div>
+  );
+}
+
+export default App;
